d40f6bcee674b70e3ac46f74657db1d7ce7d0ad1
Leandro Emanuel Diaz <leandrodiaz042@gmail.com>
2024-04-15 16:22:22 -0300

    update

---
+++ april/003_index.ts
@@ -0,0 +1,18 @@
+// https://eloquentjavascript.net/04_data.html#i-6xTmjj4Rf5
+// Reversing an array
+
+// Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, should take an array as argument and produce a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, should do what the reverse method does: modify the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+// Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+
+// // Your code here.
+
+// let myArray = ["A", "B", "C"];
+// console.log(reverseArray(myArray));
+// // → ["C", "B", "A"];
+// console.log(myArray);
+// // → ["A", "B", "C"];
+// let arrayValue = [1, 2, 3, 4, 5];
+// reverseArrayInPlace(arrayValue);
+// console.log(arrayValue);
+// // → [5, 4, 3, 2, 1]
