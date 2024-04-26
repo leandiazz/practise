564029b483f479e66da244772dd2991a06f2e132
Leandro Emanuel Diaz <leandrodiaz042@gmail.com>
2024-04-26 13:53:50 -0300

    Update: sum of a range function

--- april/002_index.ts
+++ april/002_index.ts
@@ -11,11 +11,41 @@
 
 // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements should go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure this also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
-// // Your code here.
+// console.log(range(1, 10)); // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(5, 2, -1)); // → [5, 4, 3, 2]
+// console.log(sum(range(1, 10))); // → 55
 
+function range(start: number, end: number) {
+    let array: number[] = []
+    for (let i = start; i <= end; i++) {
+        array.push(i)
+    }
+    return array
+}
 // console.log(range(1, 10));
-// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(5, 2, -1));
-// // → [5, 4, 3, 2]
+function sum(array: number[]){
+    let sum: number = 0
+    for (let i = 0; i <= array.length; i++) {
+        sum = sum + i
+    }
+    return sum
+}
 // console.log(sum(range(1, 10)));
-// // → 55
+
+
+function range_2(start: number, end: number, step: number) {
+    let array: number[] = []
+    if(Math.sign(step) === 1) {
+        for (let index = start; index <= end; index = index+step) {
+            array.push(index)
+        }
+    } else {
+        for (let index = start; index >= end; index = index+step) {
+            array.push(index)
+        }
+    }
+    return array
+}
+
+// console.log(range_2(1, 10, 2));
+// console.log(range_2(5, 2, -1)) 
